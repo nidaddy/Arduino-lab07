a341b50d45eb724da6b70f3adac8a62ccba278fc
nidaddy <jhg2333@gmail.com>
2017-05-15 17:25:33 +0800

    Add files via upload

---
+++ question1.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal.h> // include the library code
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // initialize interface pins
+void setup() {
+
+lcd.begin(20,2);
+
+lcd.print("04422420 JiangHaoge           "); // Print a message to the LCD.
+
+lcd.print("04052431 lijiaxiang     ");
+lcd.setCursor(1,0);
+
+}
+void loop() {
+// set the cursor to column 0, line 1
+// (note: line 1 is the second row, since countingbegins with 0):
+lcd.setCursor(0, 1);
+//lcd.print(millis()/1000); // print the number of secondssince reset:
+}
---
+++ question2.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal.h> // include the library code
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // initialize interface pins
+void setup() {
+lcd.begin(20, 2); // set up the LCD's number of columns and rows:
+Serial.begin(9600);
+ lcd.clear();
+
+}
+void loop() {
+ if(Serial.available())
+  {
+    char val;
+    val=Serial.read();
+     lcd.write(val);   
+    
+   
+  } 
+}
+
